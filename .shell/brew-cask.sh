ec105af9d3e450c40ff70449583ed0c8c26cf587
Gusti Azis <gooazs@gmail.com>
2016-09-09 03:48:45 +0700

    brew-cask: Remark aerial and id3-editor

--- .shell/brew-cask.sh
+++ .shell/brew-cask.sh
@@ -32,7 +32,7 @@ brew cask install qlvideo
 brew cask install provisionql
 
 # Install Apps.
-brew cask install aerial
+# brew cask install aerial
 brew cask install appcleaner
 brew cask install cheatsheet
 # brew cask install cleartext
@@ -40,7 +40,7 @@ brew cask install cheatsheet
 brew cask install flash-player
 # brew cask install icons8
 brew cask install handbrake
-brew cask install id3-editor
+# brew cask install id3-editor
 brew cask install imagealpha
 brew cask install imageoptim
 brew cask install inkscape
