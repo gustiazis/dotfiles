77d53c764808ba3c9b3efb7c569fb966c1717007
Gusti Azis <gooazs@gmail.com>
2017-04-02 19:13:56 +0700

    brew.sh: Reorder

--- .shell/brew-cask.sh
+++ .shell/brew-cask.sh
@@ -21,8 +21,8 @@ brew tap caskroom/fonts
 brew cask install qlcolorcode
 brew cask install qlstephen
 brew cask install qlmarkdown
-brew cask install quicklook-json
 brew cask install qlprettypatch
+brew cask install quicklook-json
 brew cask install quicklook-csv
 brew cask install betterzipql
 # brew cask install qlimagesize
