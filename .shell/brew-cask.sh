413b7e1b305520151f3423879541dc708bf48656
Gusti Azis <gooazs@gmail.com>
2019-07-28 17:48:30 +0700

    General: Update bash_path and brew_cask

--- .shell/brew-cask.sh
+++ .shell/brew-cask.sh
@@ -28,15 +28,17 @@ brew cask install provisionql
 brew cask install quicklookapk
 
 # Install Apps.
+brew tap AdoptOpenJDK/openjdk
+brew cask install adoptopenjdk8
 # brew cask install aerial
 brew cask install android-studio
 brew cask install appcleaner
 brew cask install cheatsheet
-# brew cask install cleartext
+brew cask install cleartext
 brew cask install cyberduck
 # brew cask install disk-doctor
 brew cask install firefox
-brew cask install genymotion
+# brew cask install genymotion
 brew cask install google-chrome
 # brew cask install handbrake
 brew cask install imagealpha
@@ -44,19 +46,17 @@ brew cask install imageoptim
 # brew cask install inkscape
 # brew cask install iterm2
 # brew cask install jpegmini
-brew cask install java
 brew cask install postman
 brew cask install robo-3t
 brew cask install sequel-pro
 brew cask install spectacle
 brew cask install spotify
-brew cask install steam
 brew cask install sublime-text
 # brew cask install teamviewer
 brew cask install the-unarchiver
 brew cask install tor-browser
 brew cask install transmission
-brew cask install virtualbox
+# brew cask install virtualbox
 brew cask install vlc
 # brew cask install xampp
 # brew cask install xquartz
