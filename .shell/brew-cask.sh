5396fa08d4c00e8eded207f1cf44a9ee4ac5b767
Gusti Azis <gooazs@gmail.com>
2020-11-30 10:13:05 +0700

    .bash_aliases: Fix Brew Upgrade Command

--- .shell/brew-cask.sh
+++ .shell/brew-cask.sh
@@ -10,7 +10,7 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 brew update
 
 # Upgrade any already-installed formulae.
-brew upgrade --all
+brew upgrade
 
 # Install Quick Look Plugins.
 brew cask install qlcolorcode
